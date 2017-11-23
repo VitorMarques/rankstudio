d12047451a2cc3f83feb91eaa128077c42e63a95
vmarques <_wuxjl28xc>
2017-11-23 14:31:49 -0200

    Correçao de bugs e melhorias

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -100,7 +100,7 @@ function realizarAgendamento(pageContext) {
         type: 'POST',
         url: url,
         data: data,
-        success: function () {Materialize.toast('Seus agendamentos foram atualizados. Clique em seu painel de controle para mais informacoes!', 8000); limpaFormAgendamento();},
+        success: function () {Materialize.toast('Seus agendamentos foram atualizados. Clique em seu painel de controle para mais informacoes!', 5000, '', window.location.reload());},
         error: function (data) {
             Materialize.toast(data.error, 4000);
         }
